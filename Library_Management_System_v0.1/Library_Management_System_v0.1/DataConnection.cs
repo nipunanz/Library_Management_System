6851a1c58bec618f9cb8373070bfa05eaa1917ff
Nipuna Fernando <nipuna.nzone@gmail.com>
2018-05-23 22:06:08 +0530

    Added mySql Connector Class

---
+++ Library_Management_System_v0.1/Library_Management_System_v0.1/DataConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Library_Management_System_v0._1
+{
+    public class DataConnection
+    {
+       static MySqlConnection mySqlConnection;
+       
+       public static MySqlConnection getDBConnection()
+        {
+            MySqlConnectionStringBuilder mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder();
+            mySqlConnectionStringBuilder.Server = "localhost";
+            mySqlConnectionStringBuilder.UserID = "root";
+            mySqlConnectionStringBuilder.Password = "123";
+            mySqlConnectionStringBuilder.Database = "nanofoxy_library_management_system";
+
+            if (DataConnection.mySqlConnection == null) {
+
+                MySqlConnection mySqlConnection = new MySqlConnection(mySqlConnectionStringBuilder.ToString());
+                mySqlConnection.Open();
+
+            }
+
+            return mySqlConnection;
+        }
+
+    }
+}
