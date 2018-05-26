4da6ad1e858ecf7dda3203c8a74552eeec68c2e0
lakshang <lakshan.1998@gmail.com>
2018-05-27 01:35:21 +0530

    Chnages to Manage_Books grid view

--- Library_Management_System_v0.1/Library_Management_System_v0.1/Manage_Books.Designer.cs
+++ Library_Management_System_v0.1/Library_Management_System_v0.1/Manage_Books.Designer.cs
@@ -35,15 +35,25 @@
             this.label2 = new System.Windows.Forms.Label();
             this.buttonEdit = new System.Windows.Forms.Button();
             this.buttonActivateDeactivate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.label1.Font = new System.Drawing.Font("Roboto Condensed", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(-2, 29);
+            this.label1.Location = new System.Drawing.Point(-2, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1287, 73);
+            this.label1.Size = new System.Drawing.Size(965, 59);
             this.label1.TabIndex = 1;
             this.label1.Text = "Manage Books";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -51,45 +61,50 @@
             // textBoxBookID
             // 
             this.textBoxBookID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxBookID.Location = new System.Drawing.Point(125, 128);
+            this.textBoxBookID.Location = new System.Drawing.Point(94, 104);
+            this.textBoxBookID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxBookID.Name = "textBoxBookID";
-            this.textBoxBookID.Size = new System.Drawing.Size(265, 28);
+            this.textBoxBookID.Size = new System.Drawing.Size(200, 24);
             this.textBoxBookID.TabIndex = 8;
             // 
             // bookId
             // 
             this.bookId.AutoSize = true;
             this.bookId.Font = new System.Drawing.Font("Roboto Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bookId.Location = new System.Drawing.Point(47, 128);
+            this.bookId.Location = new System.Drawing.Point(35, 104);
+            this.bookId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bookId.Name = "bookId";
-            this.bookId.Size = new System.Drawing.Size(72, 26);
+            this.bookId.Size = new System.Drawing.Size(58, 21);
             this.bookId.TabIndex = 7;
             this.bookId.Text = "Book ID";
             // 
             // textBoxBookName
             // 
             this.textBoxBookName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxBookName.Location = new System.Drawing.Point(537, 128);
+            this.textBoxBookName.Location = new System.Drawing.Point(403, 104);
+            this.textBoxBookName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxBookName.Name = "textBoxBookName";
-            this.textBoxBookName.Size = new System.Drawing.Size(265, 28);
+            this.textBoxBookName.Size = new System.Drawing.Size(200, 24);
             this.textBoxBookName.TabIndex = 10;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Roboto Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(428, 128);
+            this.label2.Location = new System.Drawing.Point(321, 104);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 26);
+            this.label2.Size = new System.Drawing.Size(81, 21);
             this.label2.TabIndex = 9;
             this.label2.Text = "Book Name";
             // 
             // buttonEdit
             // 
             this.buttonEdit.Font = new System.Drawing.Font("Roboto Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEdit.Location = new System.Drawing.Point(1119, 123);
+            this.buttonEdit.Location = new System.Drawing.Point(839, 100);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonEdit.Name = "buttonEdit";
-            this.buttonEdit.Size = new System.Drawing.Size(107, 33);
+            this.buttonEdit.Size = new System.Drawing.Size(80, 27);
             this.buttonEdit.TabIndex = 20;
             this.buttonEdit.Text = "Edit";
             this.buttonEdit.UseVisualStyleBackColor = true;
@@ -97,18 +112,81 @@
             // buttonActivateDeactivate
             // 
             this.buttonActivateDeactivate.Font = new System.Drawing.Font("Roboto Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonActivateDeactivate.Location = new System.Drawing.Point(938, 123);
+            this.buttonActivateDeactivate.Location = new System.Drawing.Point(704, 100);
+            this.buttonActivateDeactivate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonActivateDeactivate.Name = "buttonActivateDeactivate";
-            this.buttonActivateDeactivate.Size = new System.Drawing.Size(175, 33);
+            this.buttonActivateDeactivate.Size = new System.Drawing.Size(131, 27);
             this.buttonActivateDeactivate.TabIndex = 21;
             this.buttonActivateDeactivate.Text = "Activate/Deactivate";
             this.buttonActivateDeactivate.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(39, 133);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(880, 426);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "ISBM";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Printed Year";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Category";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Author";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Type";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
             // Manage_Books
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1283, 703);
+            this.ClientSize = new System.Drawing.Size(962, 571);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonActivateDeactivate);
             this.Controls.Add(this.buttonEdit);
             this.Controls.Add(this.textBoxBookName);
@@ -116,8 +194,10 @@
             this.Controls.Add(this.textBoxBookID);
             this.Controls.Add(this.bookId);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Manage_Books";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,5 +212,13 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button buttonEdit;
         private System.Windows.Forms.Button buttonActivateDeactivate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
     }
 }
--- Library_Management_System_v0.1/Library_Management_System_v0.1/Manage_Users.Designer.cs
+++ Library_Management_System_v0.1/Library_Management_System_v0.1/Manage_Users.Designer.cs
@@ -36,13 +36,13 @@
             this.textBoxBookID = new System.Windows.Forms.TextBox();
             this.bookId = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,9 +50,10 @@
             // 
             this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.label1.Font = new System.Drawing.Font("Roboto Condensed", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(-2, 21);
+            this.label1.Location = new System.Drawing.Point(-2, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1287, 73);
+            this.label1.Size = new System.Drawing.Size(965, 59);
             this.label1.TabIndex = 2;
             this.label1.Text = "Manage Users";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -60,9 +61,10 @@
             // buttonActivateDeactivate
             // 
             this.buttonActivateDeactivate.Font = new System.Drawing.Font("Roboto Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonActivateDeactivate.Location = new System.Drawing.Point(943, 130);
+            this.buttonActivateDeactivate.Location = new System.Drawing.Point(707, 106);
+            this.buttonActivateDeactivate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonActivateDeactivate.Name = "buttonActivateDeactivate";
-            this.buttonActivateDeactivate.Size = new System.Drawing.Size(175, 33);
+            this.buttonActivateDeactivate.Size = new System.Drawing.Size(131, 27);
             this.buttonActivateDeactivate.TabIndex = 28;
             this.buttonActivateDeactivate.Text = "Activate/Deactivate";
             this.buttonActivateDeactivate.UseVisualStyleBackColor = true;
@@ -70,9 +72,10 @@
             // buttonEdit
             // 
             this.buttonEdit.Font = new System.Drawing.Font("Roboto Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEdit.Location = new System.Drawing.Point(1124, 130);
+            this.buttonEdit.Location = new System.Drawing.Point(843, 106);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonEdit.Name = "buttonEdit";
-            this.buttonEdit.Size = new System.Drawing.Size(107, 33);
+            this.buttonEdit.Size = new System.Drawing.Size(80, 27);
             this.buttonEdit.TabIndex = 27;
             this.buttonEdit.Text = "Edit";
             this.buttonEdit.UseVisualStyleBackColor = true;
@@ -80,36 +83,40 @@
             // textBoxBookName
             // 
             this.textBoxBookName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxBookName.Location = new System.Drawing.Point(542, 135);
+            this.textBoxBookName.Location = new System.Drawing.Point(406, 110);
+            this.textBoxBookName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxBookName.Name = "textBoxBookName";
-            this.textBoxBookName.Size = new System.Drawing.Size(265, 28);
+            this.textBoxBookName.Size = new System.Drawing.Size(200, 24);
             this.textBoxBookName.TabIndex = 25;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Roboto Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(433, 135);
+            this.label2.Location = new System.Drawing.Point(325, 110);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 26);
+            this.label2.Size = new System.Drawing.Size(77, 21);
             this.label2.TabIndex = 24;
             this.label2.Text = "User Name";
             // 
             // textBoxBookID
             // 
             this.textBoxBookID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxBookID.Location = new System.Drawing.Point(130, 135);
+            this.textBoxBookID.Location = new System.Drawing.Point(98, 110);
+            this.textBoxBookID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxBookID.Name = "textBoxBookID";
-            this.textBoxBookID.Size = new System.Drawing.Size(265, 28);
+            this.textBoxBookID.Size = new System.Drawing.Size(200, 24);
             this.textBoxBookID.TabIndex = 23;
             // 
             // bookId
             // 
             this.bookId.AutoSize = true;
             this.bookId.Font = new System.Drawing.Font("Roboto Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bookId.Location = new System.Drawing.Point(52, 135);
+            this.bookId.Location = new System.Drawing.Point(39, 110);
+            this.bookId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bookId.Name = "bookId";
-            this.bookId.Size = new System.Drawing.Size(67, 26);
+            this.bookId.Size = new System.Drawing.Size(54, 21);
             this.bookId.TabIndex = 22;
             this.bookId.Text = "User ID";
             // 
@@ -126,54 +133,13 @@
             this.Column5,
             this.Column6,
             this.Column7});
-            this.dataGridView1.Location = new System.Drawing.Point(57, 212);
+            this.dataGridView1.Location = new System.Drawing.Point(43, 172);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1174, 438);
+            this.dataGridView1.Size = new System.Drawing.Size(880, 356);
             this.dataGridView1.TabIndex = 29;
-            // 
-            // Column7
-            // 
-            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column7.HeaderText = "Status";
-            this.Column7.Name = "Column7";
-            this.Column7.ReadOnly = true;
-            // 
-            // Column6
-            // 
-            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column6.HeaderText = "Reg. Date";
-            this.Column6.Name = "Column6";
-            this.Column6.ReadOnly = true;
-            // 
-            // Column5
-            // 
-            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column5.HeaderText = "Address";
-            this.Column5.Name = "Column5";
-            this.Column5.ReadOnly = true;
-            // 
-            // Column4
-            // 
-            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column4.HeaderText = "Mobile";
-            this.Column4.Name = "Column4";
-            this.Column4.ReadOnly = true;
-            // 
-            // Column3
-            // 
-            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column3.HeaderText = "Last Name";
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
-            // 
-            // Column2
-            // 
-            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column2.HeaderText = "First Name";
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
             // 
             // Column1
             // 
@@ -182,11 +148,53 @@
             this.Column1.Name = "Column1";
             this.Column1.ReadOnly = true;
             // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "First Name";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Last Name";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Mobile";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "Address";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.HeaderText = "Reg. Date";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column7.HeaderText = "Status";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
             // Manage_Users
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1283, 703);
+            this.ClientSize = new System.Drawing.Size(962, 571);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonActivateDeactivate);
             this.Controls.Add(this.buttonEdit);
@@ -195,6 +203,7 @@
             this.Controls.Add(this.textBoxBookID);
             this.Controls.Add(this.bookId);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Manage_Users";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.Manage_Users_Load);
